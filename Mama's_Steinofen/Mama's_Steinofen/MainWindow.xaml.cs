5c98402d7cdb79702362a76b30bfed54d702a243
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2023-01-13 12:57:22 +0100

    test
    test

--- Mama's_Steinofen/Mama's_Steinofen/MainWindow.xaml.cs
+++ Mama's_Steinofen/Mama's_Steinofen/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Mama_s_Steinofen
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
