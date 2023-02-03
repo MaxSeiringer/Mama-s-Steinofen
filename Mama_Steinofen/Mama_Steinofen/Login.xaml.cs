045fcb6ffe8d0f735e5bbf2b90181358a66012e5
chrisirussner <russner.christian@hak-liezen.at>
2023-02-03 11:38:07 +0100

    WPF
    Login

--- Mama_Steinofen/Mama_Steinofen/Login.xaml.cs
+++ Mama_Steinofen/Mama_Steinofen/Login.xaml.cs
@@ -23,5 +23,14 @@ namespace Mama_Steinofen
         {
             InitializeComponent();
         }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow objMainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();
+        }
     }
 }
