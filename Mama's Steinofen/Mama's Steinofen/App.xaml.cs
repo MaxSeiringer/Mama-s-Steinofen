b58a6439cf2ee1d7ea64d5d33404ffd8132dfda3
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2023-02-03 12:10:37 +0100

    Wpf
    Neu

--- Mama's Steinofen/Mama's Steinofen/App.xaml.cs
+++ Mama's Steinofen/Mama's Steinofen/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Mama_Steinofen
+namespace Mama_s_Steinofen
 {
     /// <summary>
     /// Interaktionslogik für "App.xaml"
--- Mama's Steinofen/Mama's Steinofen/Properties/AssemblyInfo.cs
+++ Mama's Steinofen/Mama's Steinofen/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Mama_Steinofen")]
+[assembly: AssemblyTitle("Mama's Steinofen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mama_Steinofen")]
+[assembly: AssemblyProduct("Mama's Steinofen")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
